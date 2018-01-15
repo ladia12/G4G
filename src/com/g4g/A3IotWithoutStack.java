26f131c52825aac73e3288f2125dcea524ccb7bd
Yash Ladia <=>
2018-01-15 19:58:03 +0530

    Inorder traversal without stack

---
+++ src/com/g4g/A3IotWithoutStack.java
@@ -0,0 +1,47 @@
+package com.g4g;
+
+import java.util.Stack;
+
+// https://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion/
+public class A3IotWithoutStack {
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    void runProgram() {
+        TreeNode root = constructTree();
+        inOrderTraversal(root);
+    }
+
+    private void inOrderTraversal(TreeNode node) {
+        Stack<TreeNode> stack = new Stack<>();
+        //Go to left child till it is possible
+        while(node != null || !stack.isEmpty()) {
+            if(node == null) {
+                node = stack.pop();
+                System.out.print(node.data + "\t");
+                node = node.right;
+            } else {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+
+    }
+
+    private TreeNode constructTree() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.right = new TreeNode(4);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        return root;
+    }
+}
--- src/com/g4g/Main.java
+++ src/com/g4g/Main.java
@@ -4,10 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        A1RootToLeaf obj1 = new A1RootToLeaf();
-        obj1.runProgram();
+//        A1RootToLeaf obj1 = new A1RootToLeaf();
+//        obj1.runProgram();
+//
+//        A2MinWindowString obj2 = new A2MinWindowString();
+//        obj2.runProgram();
 
-        A2MinWindowString obj2 = new A2MinWindowString();
-        obj2.runProgram();
+        A3IotWithoutStack obj3 = new A3IotWithoutStack();
+        obj3.runProgram();
     }
 }
